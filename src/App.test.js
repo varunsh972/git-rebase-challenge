3cb510497a46836c9435c5a06354d80317f9a978
Alex Hughes <alexhughes@AlexMBPro.local>
2021-05-06 14:09:10 -0500

    feat: add Biology Today article

--- src/App.test.js
+++ src/App.test.js
@@ -7,8 +7,16 @@ test('renders My really cool blog site logo', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+
 test('renders Auto Review Article', () => {
   render(<App />);
   const linkElement = screen.getByText(/Auto Review:/i);
+
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Biology Today article', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Biology Today:/i);
+
   expect(linkElement).toBeInTheDocument();
 });
